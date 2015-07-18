2ae8f51738dc4b7e061b9228e7dd206b27f7c8b0
ngzhongqin <ngzhongqin@gmail.com>
2015-07-18 19:20:00 +0800

    first

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+ $(document).foundation();
+/**
+ * @ngdoc overview
+ * @name winestoryApp
+ * @description
+ * # winestoryApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('winestoryApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

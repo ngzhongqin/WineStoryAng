fb0e22a7163695570ccb6c5930bfa7dc08408a4a
ngzhongqin <ngzhongqin@gmail.com>
2015-07-18 23:02:29 +0800

    signup js able to reach server side

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -8,10 +8,33 @@
  * Controller of the winestoryApp
  */
 
-app.controller('SignUpCtrl', function ($scope) {
+app.controller('SignUpCtrl', function ($scope, $http) {
 
 	$scope.submitPost = function () {
 		alert("submitPost");
+          var data = {
+              'email' : "test_email",
+              'password' : "test_pasword"
+          };
+
+          // var res = $http.post('/http://localhost:8080/login', data);
+
+          var req = {
+           method: 'POST',
+           url: 'http://localhost:8080/signup',
+           headers: {
+             'Content-Type': "application/json"
+           },
+           data:{test: 'tessdfasdf sdafsat'}
+          }
+
+          $http(req).success(function (data, status, headers, config) {
+                $scope.signup.email = data.email; 
+                $scope.signup.full_name = data.username; 
+            }).error(function (data, status, headers, config) {
+                $scope.status = status + ' ' + headers;
+            });
+
 	};
 
 });

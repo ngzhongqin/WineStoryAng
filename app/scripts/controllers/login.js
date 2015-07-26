461406153b7da8fb8dbc5f746f80fb24774b15f3
ngzhongqin <ngzhongqin@gmail.com>
2015-07-27 00:12:01 +0800

    Login Flow
    Login Flow

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,11 +7,38 @@
  * # LoginCtrl
  * Controller of the winestoryApp
  */
-angular.module('winestoryApp')
-  .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+app.controller('LoginCtrl', function ($scope, $http) {
+
+	$scope.submitPost = function () {
+    console.log("LoginCtrl submitPost");
+          var data = {
+              'email' : $scope.login.email,
+              'password' : $scope.login.password,
+          };
+
+          var req = {
+           method: 'POST',
+           url: 'http://localhost:8080/login',
+           headers: {
+             'Content-Type':  "text/plain"
+           },
+           data:{data: data}
+          }
+
+
+console.log("LoginCtrl submitPost req email:"+req.data.data.email);
+console.log("LoginCtrl submitPost req password:"+req.data.data.password);
+
+
+          $http(req).success(function (data, status, headers, config) {
+                $scope.signup.email = data.email; 
+                $scope.signup.full_name = data.full_name; 
+                $scope.signup.password = data.password; 
+            }).error(function (data, status, headers, config) {
+                $scope.status = status + ' ' + headers;
+            });
+
+	};
+
+});

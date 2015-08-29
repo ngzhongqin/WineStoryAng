4bafd93e0ff1b1634672d08f8b9527788c88bc75
ngzhongqin <ngzhongqin@gmail.com>
2015-08-29 22:58:32 +0800

    Calling UserService only if user not available

--- app/scripts/services/UserService2.js
+++ app/scripts/services/UserService2.js
@@ -2,15 +2,17 @@ app.service('UserService2',['$http','$cookies','$rootScope', function($http,$coo
     this.user = getUser;
 
     function getUser(callback) {
-        console.log("UserService2 - getUser");
-        var session_id = $cookies.get('winestory_session');
-        var req_url = backendHostname+'/session?action=GetCurrentUser'+'&'+'session_id='+session_id;     
-        $http({
-            url: req_url,
-            method: 'POST'
-        }).success(function (data, status, header, config){
-            callback(data.user);
-            $rootScope.user = data.user;
-        });
+        if($rootScope.user == null){
+            console.log("UserService2 - getUser");
+            var session_id = $cookies.get('winestory_session');
+            var req_url = backendHostname+'/session?action=GetCurrentUser'+'&'+'session_id='+session_id;     
+            $http({
+                url: req_url,
+                method: 'POST'
+            }).success(function (data, status, header, config){
+                callback(data.user);
+                $rootScope.user = data.user;
+            });
+        }
     };
 }]);
